ccdadaf98bb2a03e0d0a7aed31aeca8e8c6ea4ba
LERA <llera1138@yandex.ru>
2024-05-03 23:47:50 +0300

    change

--- src/test/java/AppOrderPositiveTest.java
+++ src/test/java/AppOrderPositiveTest.java
@@ -1,8 +1,52 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppOrderPositiveTest {
     private WebDriver driver;
 
     @BeforeAll
     public static void setupAll() {
-        WebDriverManager
+        WebDriverManager.chromedriver().setup();
     }
+
+    @BeforeEach
+    public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+        driver = null;
+    }
+    
+    @Test
+    public void shouldBeSuccessfulForm() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Капралова Диана-Диановна");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79629450885");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualTextElement = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        var actualText = actualTextElement.getText().trim();
+        assertEquals(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+        assertTrue(actualTextElement.isDisplayed());
+    }
+    
 }
